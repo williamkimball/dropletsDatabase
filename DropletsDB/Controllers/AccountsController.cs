2bb490cacbeefefce27946e03b95c2d48e01ec8e
William Kimball <wkkimball043@gmail.com>
2018-10-09 16:09:37 -0500

    built database?

---
+++ DropletsDB/Controllers/AccountsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DropletsDB.Models;
+
+namespace DropletsDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly DropletsDBContext _context;
+
+        public AccountsController(DropletsDBContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/Accounts
+        [HttpGet]
+        public IEnumerable<Account> GetAccount()
+        {
+            return _context.Account;
+        }
+
+        // GET: api/Accounts/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAccount([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var account = await _context.Account.FindAsync(id);
+
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(account);
+        }
+
+        // PUT: api/Accounts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAccount([FromRoute] int id, [FromBody] Account account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != account.AccountId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(account).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AccountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Accounts
+        [HttpPost]
+        public async Task<IActionResult> PostAccount([FromBody] Account account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Account.Add(account);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAccount", new { id = account.AccountId }, account);
+        }
+
+        // DELETE: api/Accounts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccount([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var account = await _context.Account.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            _context.Account.Remove(account);
+            await _context.SaveChangesAsync();
+
+            return Ok(account);
+        }
+
+        private bool AccountExists(int id)
+        {
+            return _context.Account.Any(e => e.AccountId == id);
+        }
+    }
+}
---
+++ DropletsDB/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DropletsDB.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
